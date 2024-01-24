573c5e68f33cedc87983d77adc61d14e7f039f18
Raymond Walugembe <94790374+Wallzray@users.noreply.github.com>
2024-01-24 10:22:35 +0300

    Updated files

--- kensc/orders.php
+++ kensc/orders.php
@@ -11,11 +11,22 @@ if(isset($_POST['order'])){
    $con= mysqli_connect('localhost','root','','kensports');
    $sql= "INSERT INTO Orders values('$contact', '$club', '$jtype', '$size');";
    $ins= mysqli_query($con,$sql);
-   if($ins){
-    echo "successful";
-   }
-   else{
-    echo mysqli_error($con);
+   
+
+    $sqlout= "SELECT * FROM  Orders;";
+    $execout= mysqli_query($con, $sqlout);
+    $n= mysqli_num_rows($execout);
+    for($i = 1; $i <= $n; $i++ ){
+        $row= mysqli_fetch_array($execout);
+        echo "
+        <table border=1>
+        <tr height='15px'>
+        <td width= '30px'>$row[0]</td>
+        <td width= '30px'>$row[1]</td>
+        <td width= '30px'>$row[2]</td>
+        <td width= '30px'>$row[3]</td>
+        </tr>
+        </table> </br>";
     }
 }  
 ?>
